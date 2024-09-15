2a389530384da76b09215e63db37ced888bc0bcd
tamemmohamed <tamemmohamed35@gmail.com>
2024-09-15 10:33:21 +0300

    create note model

--- lib/widgets/custom_note_item.dart
+++ lib/widgets/custom_note_item.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:notes_app/views/edit_note_view.dart';
 import 'package:notes_app/widgets/edit_note_view_body.dart';
 
 class CustomNoteItem extends StatelessWidget {
