278364392d67d5755f9bbf70dbcb67c329efb6e7
tamemmohamed <tamemmohamed35@gmail.com>
2024-09-19 09:57:23 +0300

    refactor multi bloc provider

--- lib/simple_bloc_observer.dart
+++ lib/simple_bloc_observer.dart
@@ -9,17 +9,17 @@ class SimpleBlocObserver implements BlocObserver {
 
   @override
   void onClose(BlocBase bloc) {
-    // TODO: implement onClose
+    debugPrint('close = $bloc');
   }
 
   @override
   void onCreate(BlocBase bloc) {
-    // TODO: implement onCreate
+    debugPrint('create = $bloc');
   }
 
   @override
   void onError(BlocBase bloc, Object error, StackTrace stackTrace) {
-    // TODO: implement onError
+    debugPrint('error = $bloc');
   }
 
   @override
